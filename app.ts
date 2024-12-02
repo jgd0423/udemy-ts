c0949fa354e939bede48d000582af916edd45fa8
Jeong Geumdam <>
2024-12-03 00:12:20 +0900

    18. 배열 타입

--- app.ts
+++ app.ts
@@ -5,6 +5,14 @@
 const person = {
     name: 'geumdam',
     age: 35,
+    hobbies:['Sports', 'Cocking'],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
