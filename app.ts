b3c9a01cadd1f7aee1e05be303b2134d485fac0b
Jeong Geumdam <>
2024-12-08 16:14:09 +0900

    23. 리터럴 타입

--- app.ts
+++ app.ts
@@ -1,15 +1,22 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-text' | 'as-number'
+) {
     let results;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        results = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        results = +input1 + +input2;
     } else {
         results = input1.toString() + input2.toString();
     }
     return results;
 }
 
-const combinedAges = combine(10, 20);
+const combinedAges = combine(10, 20, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('a', 'b');
+const combinedStringAges = combine('19',  '23', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('a', 'b', 'as-text');
 console.log(combinedNames);
