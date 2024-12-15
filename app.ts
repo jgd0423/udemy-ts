9e40c8ef309b4babb8c51e40fc7ecc7283a74485
Jeong Geumdam <>
2024-12-15 16:05:25 +0900

    29. '알 수 없는' 타입

--- app.ts
+++ app.ts
@@ -1,25 +1,9 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-    console.log('Result: ' + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb:(num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
-
-printResult(add(1, 2));
-
-let combineValues:(a: number, b:  number) => number;
-
-combineValues = add;
-// combineValues = printResult;
-
-console.log(combineValues(2, 3));
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-})
